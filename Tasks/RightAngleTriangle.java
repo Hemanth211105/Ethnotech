136e6bd0c450672373274a0ae542b111a0d215f5
Hemanth211105 <hemanthreddy.nani21@gmail.com>
2026-05-27 10:59:33 +0530

    RightAngleTriangle.java

--- Tasks/RightAngleTriangle.java
+++ Tasks/RightAngleTriangle.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
 class RightAngleTriangle {
 
 	public static void main(String[] args) {
-		int n=5;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the Number: ");
+		int n=sc.nextInt();
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<=i;j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		sc.close();
 	}
 
 }
