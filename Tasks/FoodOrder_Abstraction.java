b135981e17be84b7ef84a94532526d081e3987c7
Hemanth211105 <hemanthreddy.nani21@gmail.com>
2026-05-30 14:47:15 +0530

    FoodOrder_Abstraction.java

--- Tasks/FoodOrder_Abstraction.java
+++ Tasks/FoodOrder_Abstraction.java
@@ -44,7 +44,7 @@ class PizzaOrder extends FoodOrder{
 		System.out.println("Amount to pay: "+(total-(total*0.05)));
 	}
 }
-class Abst_Example_FoodOrder{
+class FoodOrder_Abstraction{
 	public static void main(String args[]){
 		PizzaOrder fo=new PizzaOrder("Revanth","Biryani",2,150);
 		PizzaOrder f1=new PizzaOrder("Bharath","Dosa",3,40);
