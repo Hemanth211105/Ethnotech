c997d8293e8671287770e991a880681062732851
Hemanth211105 <hemanthreddy.nani21@gmail.com>
2026-05-30 14:43:08 +0530

    Add files via upload

---
+++ Tasks/BookingCab_Polymorphism.java
@@ -0,0 +1,36 @@
+ class Person{
+	public void bookcab(){
+		System.out.println("Booking Cab..");
+	} 
+}
+class Auto extends Person{
+	@Override
+	public void bookcab(){
+		double amount=150;
+		System.out.println("For 2km Auto Charged "+amount+" rs.");
+	}
+}
+class Cab extends Person{
+	@Override
+	public void bookcab(){
+		double amount=250;
+		System.out.println("For 3km Cab Charged "+amount+" rs.");
+	}
+}
+class Bike extends Person{
+	@Override
+	public void bookcab(){
+		double amount=100;
+		System.out.println("For 1km Bike Charged "+amount+" rs.");
+	}
+}
+class BookingCab_Polymorphism{
+	public static void main(String args[]){
+		Auto a=new Auto();
+		Cab c=new Cab();
+		Bike b=new Bike();
+		a.bookcab();
+		b.bookcab();
+		c.bookcab();
+	}
+}
