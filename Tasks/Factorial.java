1a6c51d84e93479edb778ec7228a15ad6cd7e41f
Hemanth211105 <hemanthreddy.nani21@gmail.com>
2026-06-01 11:07:19 +0530

    Add files via upload

---
+++ Tasks/Factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Factorial{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int res=fact(n);
+		System.out.println(res);
+	}
+	static int fact(int n){
+		if(n<=1) return 1;
+		return n*fact(n-1);
+	}
+}
